9496c368e18ff6ec9f212ae6a9b4d95fb581ff65
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2016-10-06 09:54:57 -0400

    Update lab6-TTS.sh

--- lab6-TTS.sh
+++ lab6-TTS.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-
 #This is a shell script for lab 6
+# Script for saying the current local time
+
+#variable that includes the date and the string literal
+NOW=$(YOUR_VARIABLE_GOES_HERE)
+
+echo $NOW
+
+#commmand for generating a TTS file and incorporating the variable
+command_goes_here
+
+#command for playing back the TTS audio file
+command_goes_here
