5e6fb8ad9426dbb9ff6ba4c3c87b5a2c73ef481e
Diego <sdiego817@hotmail.com>
2016-10-16 03:58:33 +0000

    lab 6 pi

--- lab6-TTS.sh
+++ lab6-TTS.sh
@@ -3,12 +3,12 @@
 # Script for saying the current local time
 
 #variable that includes the date and the string literal
-NOW=$(YOUR_VARIABLE_GOES_HERE)
+NOW=$(date +"Ground Control time is %M minutes past %l %p U T C")
 
 echo $NOW
 
 #commmand for generating a TTS file and incorporating the variable
-command_goes_here
+pico2wave -w lab6-TTS.wav "$NOW"
 
 #command for playing back the TTS audio file
-command_goes_here
+aplay lab6-TTS.wav
